f05a14c2b8acdca069bd4a12324accc4ea4f987e
armogedonregis <armag171691@gmail.com>
2023-11-02 19:18:26 +0300

    fix subdoment

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/spb/:path*',
+        destination: 'https://spb.45.9.41.151:3000/:path*',
+      },
+    ]
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,

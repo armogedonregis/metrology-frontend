61df42f2e047ab32afe29f9c8a3425fbe20aa42c
armogedonregis <armag171691@gmail.com>
2023-11-05 01:03:08 +0300

    fix

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const isServer = dev ? "http://localhost:4000" : "http://45.9.41.151:4000";
+export const isServer = dev ? "http://localhost:4000" : "https://metrologiya.org";

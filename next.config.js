9b90cefd9f4733c7a64cb3b3c4518230b79e9f4f
armogedonregis <armag171691@gmail.com>
2023-11-06 16:56:17 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/site",
+        destination: "https://metrologiya.org",
+      },
+      {
+        // fallback
+        source: '/spb',
+        destination: 'https://spb.metrologiya.org',
+      }
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
